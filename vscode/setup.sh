02c524ffccac0cb93c09b87e92a1354001a67323
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2023-07-10 18:40:28 +0900

    Do nothing for vscode in Linux

--- vscode/setup.sh
+++ vscode/setup.sh
@@ -2,5 +2,14 @@
 script_name=$0
 script_dir=$(dirname "$0")
 
-ln -h ${script_dir}/settings.json ~/Library/Application\ Support/Code/User/settings.json
-ln -h ${script_dir}/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+uname=`uname`
+
+case $uname in
+  Darwin)
+    ln -h ${script_dir}/settings.json ~/Library/Application\ Support/Code/User/settings.json
+    ln -h ${script_dir}/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+    ;;
+  Linux)
+    # Do nothing
+    ;;
+esac
